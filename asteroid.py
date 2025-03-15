cc12fcb33604d8694b1c861b8ca428ec51f31d10
coding920 <justin.tamez04@gmail.com>
2025-03-14 23:42:56 -0700

    Implemented Asteroid.split() function

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 from circleshape import *
+from constants import *
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -9,3 +11,13 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        angle = random.uniform(20, 50)
+        a1 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+        a2 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+        a1.velocity = self.velocity.rotate(angle) * 1.2
+        a2.velocity = self.velocity.rotate(-angle) * 1.2
--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collided(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
             if asteroid.collided(player):
